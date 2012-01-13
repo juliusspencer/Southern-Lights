99a7c42add32472161bc7c272233ea9863c97b18
Julius Spencer <jspencer@juliusspencer.co.nz>
2012-01-14 11:12:20 +1300

    ignored some files and removed some unnecessary code

--- src/org/gtugs/auckland/usb/led/LedControllerImpl.java
+++ src/org/gtugs/auckland/usb/led/LedControllerImpl.java
@@ -6,16 +6,6 @@ import org.gtugs.auckland.usb.UsbAccessoryController;
 
 public class LedControllerImpl implements LedControllerInterface {
 
-	public static final int LED_1 = 0; 
-	public static final int LED_2 = 1; 
-	public static final int LED_3 = 2; 
-	public static final int LED_4 = 3; 
-	public static final int LED_5 = 4; 
-	public static final int LED_6 = 5; 
-	public static final int LED_7 = 6; 
-	public static final int LED_8 = 7; 
-	public static final int LED_9 = 8; 
-	
 	private final int ledId;
 	private final UsbAccessoryController controller;
 
@@ -29,55 +19,6 @@ public class LedControllerImpl implements LedControllerInterface {
 		
 		controller.sendCommand(LED_SERVO_COMMAND, (byte) (ledId), value);
 
-//		switch (colour) {
-//		case LED_1:
-//			controller.sendCommand(LED_SERVO_COMMAND, (byte) (ledId+1), value);
-//			
-//			break;
-//
-//		case LED_2:
-//			controller.sendCommand(LED_SERVO_COMMAND, (byte) (ledId+1), value);
-//			
-//			break;
-//
-//		case LED_3:
-//			controller.sendCommand(LED_SERVO_COMMAND, (byte) (ledId+1), value);
-//			
-//			break;
-//
-//		case LED_4:
-//			controller.sendCommand(LED_SERVO_COMMAND, (byte) (ledId+1), value);
-//			
-//			break;
-//
-//		case LED_5:
-//			controller.sendCommand(LED_SERVO_COMMAND, (byte) (ledId+1), value);
-//			
-//			break;
-//
-//		case LED_6:
-//			controller.sendCommand(LED_SERVO_COMMAND, (byte) (ledId+1), value);
-//			
-//			break;
-//
-//		case LED_7:
-//			controller.sendCommand(LED_SERVO_COMMAND, (byte) (ledId+1), value);
-//			
-//			break;
-//
-//		case LED_8:
-//			controller.sendCommand(LED_SERVO_COMMAND, (byte) (ledId+1), value);
-//			
-//			break;
-//
-//		case LED_9:
-//			controller.sendCommand(LED_SERVO_COMMAND, (byte) (ledId+1), value);
-//			
-//			break;
-//
-//		default:
-//			break;
-//		}
 	}
 	
 	public static void setLed(LedControllerGroup leds, boolean isChecked, int ledId, int intensity) {
